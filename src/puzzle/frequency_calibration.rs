a9542eb61e7c6982dd738e73b8e51f2eadcee7ce
AmaranthineCodices <git@amaranthinecodices.me>
2018-12-02 22:34:36 -0500

    rustfmt frequency_calibration

--- src/puzzle/frequency_calibration.rs
+++ src/puzzle/frequency_calibration.rs
@@ -1,6 +1,6 @@
 use regex::Regex;
 
-use ::Puzzle;
+use Puzzle;
 
 lazy_static! {
     static ref OFFSET_REGEX: Regex = Regex::new(r"([+-]\d+)").unwrap();
@@ -10,15 +10,11 @@ pub struct FrequencyCalibration;
 
 impl Puzzle for FrequencyCalibration {
     fn solve(&self, input: &str) -> String {
-        let mut correction = 0;
-
-        for capture in OFFSET_REGEX.captures_iter(input) {
-            let offset_string = capture.get(1).unwrap().as_str();
-            let offset_number = offset_string.parse::<isize>().unwrap();
-            correction += offset_number;
-        }
-
-        correction.to_string()
+        OFFSET_REGEX
+            .captures_iter(input)
+            .map(|c| c.get(1).unwrap().as_str().parse::<isize>().unwrap())
+            .sum::<isize>()
+            .to_string()
     }
 }
 
@@ -26,7 +22,10 @@ pub struct FrequencyDuplication;
 
 impl Puzzle for FrequencyDuplication {
     fn solve(&self, input: &str) -> String {
-        let offsets: Vec<isize> = OFFSET_REGEX.captures_iter(input).map(|c| c.get(1).unwrap().as_str().parse::<isize>().unwrap()).collect();
+        let offsets: Vec<isize> = OFFSET_REGEX
+            .captures_iter(input)
+            .map(|c| c.get(1).unwrap().as_str().parse::<isize>().unwrap())
+            .collect();
         let final_frequency: isize = offsets.iter().sum();
         let seen_frequencies: Vec<isize> = {
             let mut current_frequency = 0;
@@ -48,7 +47,7 @@ impl Puzzle for FrequencyDuplication {
 
                 for seen_frequency in &seen_frequencies {
                     if current_frequency == *seen_frequency {
-                        return current_frequency.to_string()
+                        return current_frequency.to_string();
                     }
                 }
             }
